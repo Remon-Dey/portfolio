2b798b8766848d47bedca64aca766c31a422f815
Remon Dey <remon.2003.dey@gmail.com>
2024-12-01 02:04:37 +0600

    footer jsx is ready

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import './footer.css'
+import { FaLinkedin } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <ul className='permalinks'>
+        <li> <a href='#'>Home</a></li> 
+        <li> <a href='#about'>About</a></li>
+        <li> <a href='#skills'>Skills</a></li>
+        <li> <a href='#recent_work'>Projects</a></li>
+        <li> <a href='#contact'>Contact</a></li> 
+      </ul>
+      <div className="footer__socials">
+      <a href="https://www.linkedin.com/in/remon-dey-89a082302/" target='main'><FaLinkedin/></a>
+      <a href='https://www.facebook.com/remon.dey.923' target='main'><FaFacebook /></a>
+      <a href='https://github.com/Remon-Dey' target='main'><FaGithub/></a> 
+      </div>
+      <div className="footer_copyright">
+        <small>&copy; Remon Dey. All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
