cacca2e09143545f76a196189a014b576e67cf64
Remon Dey <remon.2003.dey@gmail.com>
2024-12-01 00:09:53 +0600

    edited nav

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -3,7 +3,8 @@ import './nav.css'
 import { HiOutlineHome } from "react-icons/hi2";
 import { AiOutlineUser } from "react-icons/ai";
 import { GiSkills } from "react-icons/gi";
-import { PiSteps } from "react-icons/pi";
+import { GoProjectSymlink } from "react-icons/go";
+// import { PiSteps } from "react-icons/pi";  ///not needed now
 import { RiContactsBook3Line } from "react-icons/ri";
 import { useState } from 'react';
 const Nav = () => {
@@ -13,7 +14,8 @@ const Nav = () => {
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><HiOutlineHome /></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href="#skills"onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''} ><GiSkills /></a>
-      <a href="#career"onClick={() => setActiveNav('#career')} className={activeNav === '#career' ? 'active' : ''} ><PiSteps /></a>
+      {/* <a href="#career"onClick={() => setActiveNav('#career')} className={activeNav === '#career' ? 'active' : ''} ><PiSteps /></a>  ////not needed now */}
+      <a href="#recent_work"onClick={() => setActiveNav('#recent_work')} className={activeNav === '#recent_work' ? 'active' : ''} ><GoProjectSymlink /></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><RiContactsBook3Line /></a>
     </nav>
   )
