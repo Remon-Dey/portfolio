ee6b7302d4ae9287d0c211dd3ed716279152db90
Remon Dey <remon.2003.dey@gmail.com>
2024-12-01 22:08:56 +0600

    Updates

--- src/components/career/Career.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './career.css'
-const Career = () => {
-  return (
-    <section id='career'>Career</section>
-  )
-}
-
-export default Career
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import App from './App'
-import { createRoot } from 'react-dom/client';
-import './index.css'
-createRoot(document.querySelector('#root')).render(<App />);
